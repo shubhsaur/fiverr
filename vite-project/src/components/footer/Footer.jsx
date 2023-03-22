afac3bf8fe1fc190c1db570700a23b8022899176
Shubham Saurabh <shubhamsaurabh@Shubhams-MacBook-Pro.local>
2023-03-22 23:07:55 +0530

    added footer and product components

--- vite-project/src/components/footer/Footer.jsx
+++ vite-project/src/components/footer/Footer.jsx
@@ -1,9 +1,110 @@
 import React from 'react';
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>fiverr</h2>
+            <span>&#169; Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="images/twitter.png" alt="" />
+              <img src="images/facebook.png" alt="" />
+              <img src="images/linkedin.png" alt="" />
+              <img src="images/pinterest.png" alt="" />
+              <img src="images/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./images/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="./images/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="./images/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
